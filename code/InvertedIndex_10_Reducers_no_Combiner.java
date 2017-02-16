11c454f83e0613f5781151a6e56db2638c12decb
paulvercoustre <paulvercoustre@gmail.com>
2017-02-16 12:47:19 +0100

    Update InvertedIndex_10_Reducers_no_Combiner.java

--- code/InvertedIndex_10_Reducers_no_Combiner.java
+++ code/InvertedIndex_10_Reducers_no_Combiner.java
@@ -2,20 +2,83 @@
 
 package inverted_index;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class InvertedIndex extends Configured implements Tool {
+   public static void main(String[] args) throws Exception {
+      System.out.println(Arrays.toString(args));
+      int res = ToolRunner.run(new Configuration(), new InvertedIndex(), args);
+      
+      System.exit(res);
+   }
 
-	@Override
-	public int run(String[] arg0) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+   @Override
+   public int run(String[] args) throws Exception {
+      System.out.println(Arrays.toString(args));
+      Job job = new Job(getConf(), "InvertedIndex");
+      job.setJarByClass(InvertedIndex.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(IntWritable.class);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+      job.setMapperClass(Map.class);
+      job.setReducerClass(Reduce.class);
+      job.setNumReduceTasks(10); // we set the number of reducers to 10
 
-	}
+      job.setInputFormatClass(TextInputFormat.class);
+      job.setOutputFormatClass(TextOutputFormat.class);
 
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+      job.waitForCompletion(true);
+      
+      return 0;
+   }
+   
+   public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+      private final static IntWritable ONE = new IntWritable(1);
+      private Text word = new Text();
+
+      @Override
+      public void map(LongWritable key, Text value, Context context)
+              throws IOException, InterruptedException {
+         for (String token: value.toString().split("\\s+")) {
+            word.set(token.toLowerCase(Locale.ENGLISH)); // we convert all strings to lower case
+            context.write(word, ONE);
+         }
+      }
+   }
+
+   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+      @Override
+      public void reduce(Text key, Iterable<IntWritable> values, Context context)
+              throws IOException, InterruptedException {
+         int sum = 0;
+         for (IntWritable val : values) {
+            sum += val.get();
+         }
+         if (sum > 4000) {              // we only write keys associated to values > 4000 on the output file
+        	 context.write(key, new IntWritable(sum)); 
+         }
+         
+      }
+   }
 }
